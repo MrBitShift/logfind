edf4d4d67c0a9b32144d36968812f3e47877a2a5
MrBitShift <44534752+MrBitShift@users.noreply.github.com>
2019-02-15 12:52:40 -0700

    test version of process_args() implemented successfully

--- logfind.c
+++ logfind.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <dbg.h>
+#include <string.h>
 
 #define HELP "./help"
 #define HELP_FLAG "--help"
+#define AND_FLAG "-a"
+#define OR_FLAG "-o"
+
+enum Flag
+{
+	And, Or
+};
 
 char* read_file(char *filename, size_t size)
 {
@@ -46,9 +54,78 @@ error:
 	return -1;
 }
 
+int process_args(int argc, char *argv[])
+{
+	int i;
+	int searches_count;
+	// default search logic is and
+	enum Flag flag = And;
+	// set length of searches and allocate
+	searches_count = argc - 1;
+	char **searches = calloc(searches_count, sizeof(int));
+	for (i = 1; i < argc; i++)
+	{
+		if (strcmp(argv[i], HELP_FLAG) == 0)
+		{
+			show_help();
+			i = argc;
+		}
+		
+		else if (strcmp(argv[i], OR_FLAG) == 0)
+		{
+			flag = Or;
+			i = argc;
+		}
+		
+		else if (strcmp(argv[i], AND_FLAG) == 0)
+		{
+			flag = And;
+			i = argc;
+		}
+		
+		else
+		{
+			// assign value to searches
+			searches[i - 1] = argv[i];
+		}
+	}
+	// trim excess values from searches
+	for (i = 0; i < searches_count; i++)
+	{
+		if (searches[i] == NULL)
+		{
+			searches = realloc(searches, i);
+			searches_count = i;
+			break;
+		}
+	}
+	
+	// now that excess is trimmed, print values
+	printf("Inputted args: \n");
+	for (i = 0; i < searches_count; i++)
+	{
+		printf("\t%s\n", searches[i]);
+	}
+	// add final newline
+	printf("\n");
+	
+	// print what logic flag is
+	if (flag == And)
+	{
+		printf("Logic flag is and.\n");
+	}
+	if (flag == Or)
+	{
+		printf("Logic flag is or.\n");
+	}
+	return 0;
+error:
+	free(searches);
+}
+
 int main(int argc, char *argv[])
 {
-	show_help();
+	process_args(argc, argv);
 	return 0;
 error:
 	
